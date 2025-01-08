ad559da2676756651985fbd000e06ed68097a7a7
AmirNak-Server <amirnakgamer@gmail.com>
2025-01-08 16:26:58 +0000

    Catch and handle ReadTimeout error

--- main.py
+++ main.py
@@ -26,27 +26,31 @@ logger.remove()
 logger.configure(**LOGS_CONFIG)
 
 
+@logger.catch
 async def main():
     logger.info("Start parsing Internships")
-    # Шаг 1: Получение количества стажировок
-    internship_count = await get_internship_count(INTERNSHIPS_PAGE_URL, PAGE_PARAM)
-    logger.debug(f"Total internships: {internship_count}")
+    try:
+        # Шаг 1: Получение количества стажировок
+        internship_count = await get_internship_count(INTERNSHIPS_PAGE_URL, PAGE_PARAM)
+        logger.debug(f"Total internships: {internship_count}")
 
-    # Шаг 2: Получение всех карточек на стажировки
-    internship_links = await get_all_internship_links(INTERNSHIPS_API_URL, API_PARAM, internship_count, OFFSET_PARAM)
-    logger.debug(f"Found {len(internship_links)} internship links")
+        # Шаг 2: Получение всех карточек на стажировки
+        internship_links = await get_all_internship_links(INTERNSHIPS_API_URL, API_PARAM, internship_count, OFFSET_PARAM)
+        logger.debug(f"Found {len(internship_links)} internship links")
 
-    # Шаг 3: Асинхронный парсинг каждой карточки стажировки
-    async with httpx.AsyncClient() as client:
-        tasks = [parse_internship_card(client, link) for link in internship_links]
-        internships_data = await asyncio.gather(*tasks)
+        # Шаг 3: Асинхронный парсинг каждой карточки стажировки
+        async with httpx.AsyncClient() as client:
+            tasks = [parse_internship_card(client, link) for link in internship_links]
+            internships_data = await asyncio.gather(*tasks)
 
-    internships_data = [item for sublist in internships_data for item in sublist]
+        internships_data = [item for sublist in internships_data for item in sublist]
 
-    # Шаг 4: Авторизация и отправка данных в Google Таблицу
-    service = authorize_google_sheets()
-    write_to_google_sheet(service, SPREADSHEET_ID, SHEET_NAME, internships_data)
-    logger.debug("Data sent to Google Sheets")
+        # Шаг 4: Авторизация и отправка данных в Google Таблицу
+        service = authorize_google_sheets()
+        write_to_google_sheet(service, SPREADSHEET_ID, SHEET_NAME, internships_data)
+        logger.debug("Data sent to Google Sheets")
+    except httpx.ReadTimeout:
+        logger.error("Nothing was found")
     logger.info("End parsing Internships")
 
 
